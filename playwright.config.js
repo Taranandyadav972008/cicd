4f87778e59381165b95d28dafc1ca33d40c3d342
Taranandyadav972008 <taranandyadav42@gmail.com>
2026-05-08 01:35:32 +0300

    Add e2e tests and fix package.json

---
+++ playwright.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './e2e-tests',
+  use: {
+    baseURL: 'http://localhost:8080',
+  },
+  webServer: {
+    command: 'npm run start',
+    port: 8080,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+});
